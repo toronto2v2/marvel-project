9d27f4d972222ebb5775778fa53f702d4e70cc04
Mykhailo <toronto2v2@icloud.com>
2022-12-13 14:51:58 +0200

    fixing bugs

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,14 +15,26 @@ class MarvelService {
 
     }
 
-    getAllCharacters = () => {
+    getAllCharacters = async() => {
 
-        return this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter)
     }
 
-    getCharacter = (id) => {
+    getCharacter = async (id) => {
+       const res = await this.getResourse(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+       return this._transformCharacter(res.data.results[0]);
+    }
 
-        return this.getResourse(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description.length === 0 ? 'Description are not available' : char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url,
+        }
     }
 }
 
