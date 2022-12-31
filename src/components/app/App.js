3617401c7cbaa14d20d9ac7d0b22f6384a8ddc18
Mykhailo <toronto2v2@icloud.com>
2022-12-31 16:37:36 +0200

    added react.lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,14 @@
+import { lazy, Suspense } from 'react';
 
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom' 
 import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage, Page404,SingleComicPage} from '../pages'
+import Spinner from '../spinner/spinner';
+
+// Dynamic imports shuld be under static imporst!!!!
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 const App = () => {
 
@@ -11,13 +18,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route path='/' element={<MainPage/>}/>
-                        <Route path='/comics' element = {<ComicsPage/>}/>
-                        <Route path='/comics/:comicId' element = {<SingleComicPage/>}/>
-                        <Route path = '*' element ={<Page404 backToWhat = 'main page' to = '/'/>}/>
-
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route path='/' element={<MainPage/>}/>
+                            <Route path='/comics' element = {<ComicsPage/>}/>
+                            <Route path='/comics/:comicId' element = {<SingleComicPage/>}/>
+                            <Route path = '*' element ={<Page404 backToWhat = 'main page' to = '/'/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
